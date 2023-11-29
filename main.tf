e49de3e9bef80261c165103a42b29b9c1dd6fb57
dc83 <dino.chhabria@gmail.com>
2023-11-29 11:17:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,8 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.38.0"
-    }
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucketadcarr134"
+    Environment = "Dev"
   }
-}
-
-terraform {
-  backend "gcs" {
-    bucket  = "dsdatsme-tfstate-store"
-    prefix  = "gh-terraform-state"
-  }
-}
-
-
-provider "google" {
-  # Configuration options
-  project = "red-alloy-290810"
-}
-
-resource "google_storage_bucket" "basic-bucket" {
-  name     = "dsdatsme1234"
-  location = "EU"
-  labels = {
-    "env" = "dev"
-  }
-  force_destroy = true
-}
-
-resource "google_storage_bucket" "basic-bucket2" {
-  name          = "dsdatsme12345"
-  location      = "EU"
-  force_destroy = true
 }
